50a1dc239e5c211e24a75162b3418bb9cd6ae52c
01jaimesg <157081115+01jaimesg@users.noreply.github.com>
2024-01-24 02:41:28 +0000

    Hello World

--- src/examples/a_example/devprocess.py
+++ src/examples/a_example/devprocess.py
@@ -3,3 +3,7 @@ def add_numbers(val1, val2): #definding the function with 2 peramiters
      '''one line
      '''
      return val1 + val2
+
+
+def  say_hello(name):
+     print("Hello World" + name)
--- src/examples/a_example/main.py
+++ src/examples/a_example/main.py
@@ -2,4 +2,7 @@ import devprocess
 
 result = devprocess.add_numbers(5, 5)
 
-print(result)
+#print(result)
+
+
+devprocess.say_hello("Jaime")

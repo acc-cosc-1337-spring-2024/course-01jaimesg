5310a2844dbc8383fd039b7a1bf8e24162c04e51
01jaimesg <157081115+01jaimesg@users.noreply.github.com>
2024-01-28 05:04:16 +0000

    Homework1

--- src/examples/b_input_proc_output/main.py
+++ src/examples/b_input_proc_output/main.py
@@ -1,29 +1,8 @@
 import input_process_output
 
-input_process_output.display_output()
+value = input("Enter Here")
 
-input_process_output.echo_value("Python")
-input_process_output.echo_value("C++")
-
-input_process_output.say_hello("Python Learners")
-
-result = input_process_output.add_values(5,5)
+result = input_process_output.add_value(int(value), int(value))
 print(result)
-
-result = "Python is flexible"
-print(result)
-
-
-result = 18.99
-result = result + 1.50
-print(result)
-
-result = "10.99"
-result = result + "1.50"
-print (result)
-
-result = "10.99"
-result = result + "1.50"
-print (result)
 
 
--- src/homework/b_in_proc_out/main.py
+++ src/homework/b_in_proc_out/main.py
@@ -1,1 +1,12 @@
-#
+import output
+
+
+result = output.multiply_numbers(5, 5)
+print(result)   
+
+
+Value1 = input("enter value 1: ")
+Value2 = input("enter value 2")
+
+result = output.multiply_numbers(int(Value1), int(Value2))
+print(result)

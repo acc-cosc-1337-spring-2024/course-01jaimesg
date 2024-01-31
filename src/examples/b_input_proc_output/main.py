855329bb12cd3dfa2c86a55196755bfab484b71e
01jaimesg <157081115+01jaimesg@users.noreply.github.com>
2024-01-31 02:20:35 +0000

    order of operations

--- src/examples/b_input_proc_output/main.py
+++ src/examples/b_input_proc_output/main.py
@@ -1,8 +1,10 @@
 import input_process_output
 
-value = input("Enter Here")
+val1 = input("enter val 1:")
+val2 = input("enter val 2:")
+val3 = input("enter val 3: ")
 
-result = input_process_output.add_value(int(value), int(value))
+result = input_process_output.operator_precedence_1(int(val1), int(val2), int(val3))
+
 print(result)
-
 
--- tests/examples/b_input_process_output/tests_input_process_output.py
+++ tests/examples/b_input_process_output/tests_input_process_output.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.examples.b_input_proc_output.input_process_output import add_values, test_config, floating_point_division
+from src.examples.b_input_proc_output.input_process_output import add_values, operator_precedence_1, test_config, floating_point_division, integer_division
 
 class Test_Config(unittest.TestCase):
 
@@ -15,4 +15,11 @@ def test_floating_point_division_1(self):
     self.assertEqual(floating_point_division(5, 2), 2.5)
 
 def test_floating_point_division_2(self):
-    self.assertEqual(floating_point_division(20, 10), 2)
+    self.assertEqual(floating_point_division(20, 8), 2.5)
+
+def test_integer_division_1(self):
+    self.assertEqual(integer_division(20, 8), 2)
+
+def test_operator_precedence_1(self):
+    self.assertEqual(operator_precedence_1(12, 6, 3), 6)
+

f086a9704aba508d57f2df5ff64d72a57c4136d2
01jaimesg <157081115+01jaimesg@users.noreply.github.com>
2024-02-07 02:51:40 +0000

    Homework2

--- src/homework/c_decisions/decisions.py
+++ src/homework/c_decisions/decisions.py
@@ -1,1 +1,22 @@
-#
+def get_options_ratio(options, total):
+    ratio = options / total
+    return ratio
+
+
+def get_faculty_rating(ratio):
+
+    result = ""
+    if (ratio <= 10 and ratio >= .9):
+        return 'Excellent'
+    elif (ratio < .9 and ratio >= .8):
+        return 'Very Good'
+    elif (ratio < .8 and ratio >= .7):
+        return 'Good'
+    elif (ratio < .7 and ratio >= .6):
+        return 'Needs Improvements'
+    elif (ratio < .6 and ratio >=0):
+        return 'Unacceptable'
+    
+    return result
+
+
--- src/homework/c_decisions/main.py
+++ src/homework/c_decisions/main.py
@@ -1,1 +1,6 @@
-#
+import decisions
+
+ratio = float(input("Total grade: "))
+result = decisions.get_faculty_rating(ratio)
+
+print(ratio, "is", result)

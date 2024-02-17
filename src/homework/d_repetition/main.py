6ba3ea48507d00434011473cb1deb65e691e0c33
01jaimesg <157081115+01jaimesg@users.noreply.github.com>
2024-02-17 21:09:54 +0000

    Homework 3

--- src/homework/d_repetition/main.py
+++ src/homework/d_repetition/main.py
@@ -1,1 +1,7 @@
-#
+import repetition
+
+#repetition.get_factorial(5)
+#repetition.sum_odd_numbers(5)
+repetition.homework_menu()
+
+
--- src/homework/d_repetition/repetition.py
+++ src/homework/d_repetition/repetition.py
@@ -1,1 +1,26 @@
-#
+#jaimesg
+
+
+def get_factorial(num):
+    sum = 0
+
+    for num in[1, 2, 6, 24, 120, 720]:
+    #for i in range(num):        
+            print(num)
+
+def sum_odd_numbers(num):
+      for i in range(1, 20, 2):
+            print(i)
+
+
+def homework_menu():
+    for word in ["Homework 3 Menu", "1 - Factoral", "2 - Sum odd numbers", "3 - Exit"]:
+        print(word)
+
+def while_validate_user_input():
+    lot_number = -1
+    
+    while(lot_number != 0):
+        lot_number = input("Enter number from 1-10")
+
+    

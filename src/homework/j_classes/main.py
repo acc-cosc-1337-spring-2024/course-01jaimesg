6d46679c0f0363b4f196b828085e8eeedfb3a4d4
01jaimesg <157081115+01jaimesg@users.noreply.github.com>
2024-04-21 01:54:41 +0000

    Homework 9

--- src/homework/j_classes/main.py
+++ src/homework/j_classes/main.py
@@ -1,1 +1,21 @@
-#
+#01jaimesg
+from class_a import Die
+
+def main():
+    die = Die()
+    while True:
+        print ("1 - roll die")
+        print ("2 - exit program")
+
+        num = input("Enter number:  ")
+        if num == "1":
+              die.roll()
+              print(die)
+        
+        elif num == "2":
+             print("Exiting")
+             break
+        
+        else:
+             print("Invalid selection. Please enter 1 or 2: ")
+main()
--- tests/homework/j_classes/tests_classes.py
+++ tests/homework/j_classes/tests_classes.py
@@ -1,1 +1,12 @@
-#
+import unittest
+
+from src.homework.j_classes.class_a import Die
+
+class Test_Config(unittest.TestCase):
+
+    def test_die(self):
+       roll = Die ()
+
+       for _ in range(3):
+        roll.roll()
+        self.assertEqual(1 <= roll.get_rolled_value()<= 6, True)
